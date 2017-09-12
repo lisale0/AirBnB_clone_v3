c3d726d71fdfeac7cb4551157ae147a203569b46
Lisa Leung <110@holbertonschool.com>
2017-09-12 18:43:23 +0000

    add 404 error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,11 +6,17 @@ from flask import Flask
 from api.v1.views import app_views
 from models import storage
 from os import getenv
+from flask import jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def notFound(error):
+    """ return 404"""
+    return jsonify({"error": "Not found"}), 404
 
+@app.teardown_appcontext
 def teardown(self):
     """ close storage session """
     storage.close()
@@ -18,4 +24,4 @@ def teardown(self):
 if __name__ == "__main__":
     _host = getenv('HBNB_API_HOST', '0.0.0.0')
     _port = getenv('HBNB_API_PORT', 5000)
-    app.run(host=_host, port=7000)
+    app.run(host=_host, port=_port)
