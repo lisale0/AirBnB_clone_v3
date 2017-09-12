eaad6ea173347c0c7d31b1e14252ed9d12e7eb29
Stuart Kuredjian <108@holbertonschool.com>
2017-09-12 02:17:06 +0000

    __init__.py : add blueprint argument 'url_prefix'

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,4 @@
 from flask import Blueprint
 from api import *
 
-app_views = Blueprint('app_views', __name__, template_folder='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
