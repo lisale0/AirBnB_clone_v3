97587937fcc3effe022dc9c0a2f486e60356d483
Stuart Kuredjian <108@holbertonschool.com>
2017-09-13 08:12:18 +0000

    states.py :create post_state route

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,15 +6,40 @@ from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
 from models.state import State
-from models.base_model import BaseModel
+# from models.base_model impoert BaseModel
 
 
-@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
-def view_one_state(state_id=None):
+@app_views.route('/states/', methods=['GET'], strict_slashes=False)
+def view_all_states():
+    """ returns all state objects in JSON format   """
+    _states = [state.to_json() for state in storage.all('State').values()]
+
+    return jsonify(_states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def view_state(state_id=None):
     """ returns a state object in JSON format  """
-    if state_id is None:
-        abort(404)
-    state = storage.get("State", state_id)
-    if state is None:
-        abort(404)
-    return jsonify(state.to_json())
+    if state_id:
+        state = storage.get('State', state_id)
+        if state:
+            return jsonify(state.to_json())
+
+    abort(404)
+
+
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+def post_state():
+    """ creates a state  """
+    json_obj = None
+    try:
+        json_obj = request.get_json()
+    except:
+        return 'Not a JSON', 400
+
+    if 'name' not in json_obj.keys():
+        return 'Missing name', 400
+
+    state = State(**json_obj)
+    state.save()
+    return jsonify(state.to_json()), 201
