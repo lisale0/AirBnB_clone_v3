3a1789c119241c13aaf4187ad34162825226d9b8
Stuart Kuredjian <108@holbertonschool.com>
2017-09-14 09:42:39 +0000

    way too tired to think of a dedcription

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -14,7 +14,7 @@ from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
-from api.v1.views.reviews import *
+from api.v1.views.places_reviews import *
 from flask import jsonify
 from models.base_model import BaseModel
 from models.amenity import Amenity
--- models/user.py
+++ models/user.py
@@ -2,26 +2,31 @@
 """
 User Class from Models Module
 """
-import os
+
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String, Float
-storage_type = os.environ.get('HBNB_TYPE_STORAGE')
+from os import environ
 
 
 class User(BaseModel, Base):
     """User class handles all application users"""
-    if storage_type == "db":
+    if 'HBNB_TYPE_STORAGE' in environ and environ['HBNB_TYPE_STORAGE'] == 'db':
         __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-
-        places = relationship('Place', backref='user', cascade='delete')
-        reviews = relationship('Review', backref='user', cascade='delete')
+        places = relationship("Place", cascade="all, delete-orphan",
+                              backref="user")
+        reviews = relationship("Review", cascade="all, delete-orphan",
+                               backref="user")
     else:
-        email = ''
-        password = ''
-        first_name = ''
-        last_name = ''
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """instantiates a new user"""
+        super().__init__(self, *args, **kwargs)
