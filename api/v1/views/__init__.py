5cd48e5101b3b31985962d49fee50882e839a9ef
Stuart Kuredjian <108@holbertonschool.com>
2017-09-14 21:11:33 +0000

    __init__.py : remove jsonify and vase model imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -16,7 +16,6 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.amenities import *
 from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
 # from flask import jsonify
 # from models.base_model import BaseModel
 from models.amenity import Amenity
