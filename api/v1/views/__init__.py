adabd7b2b9bb1603628d997408d9705f4663c1c3
Stuart Kuredjian <108@holbertonschool.com>
2017-09-14 23:10:29 +0000

    __init__.py :remove jsonify and base model imports

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -16,8 +16,6 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.amenities import *
 from api.v1.views.places_reviews import *
-# from flask import jsonify
-# from models.base_model import BaseModel
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
