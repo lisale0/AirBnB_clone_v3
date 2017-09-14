1046e85f52da8c242def755d6834bc0df5ebc7c3
Lisa Leung <110@holbertonschool.com>
2017-09-14 17:34:46 +0000

    inital add to implement cross origin http request

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,9 +7,11 @@ from api.v1.views import app_views
 from models import storage
 from os import getenv
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
