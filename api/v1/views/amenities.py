dcc3d8c2035c5b9b1dbdf2bf3b2c5e279afeac70
Lisa Leung <110@holbertonschool.com>
2017-09-14 07:52:54 +0000

    initial add amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,58 +3,62 @@
 module: State api
 """
 from api.v1.views import app_views
-from flask import jsonify
-from flask import abort
-from models.base_model import BaseModel
-from models.state import State
-from models.user import User
+from flask import jsonify, abort, request
 from models.amenity import Amenity
 from models import storage
-from flask import request
 
 
-@app_views.route('/states/<string:state_id>/cities', strict_slashes=False)
-def get_amenities_byState(state_id):
-    """ returns cities: return all cities from
-    specified state in json format  """
-    all_cities = storage.all("City")
-    state = storage.get("State", state_id)
-    if state is None:
+@app_views.route('/amenities/', methods=['GET'], strict_slashes=False)
+def get_amenities():
+    """ returns all amenities """
+    _amenities = [amenity.to_json()
+                  for amenity in storage.all('Amenity').values()]
+    return jsonify(_amenities)
+
+
+@app_views.route('/amenities/<string:amenity_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_amenities_byID(amenity_id=None):
+    """ returns amenity by id """
+    amenity = storage.get("Amenity", amenity_id)
+    if amenity is None:
         abort(404)
-    json_array = []
-    for k, v in all_cities.items():
-        if (v.to_json()['state_id'] == state_id):
-            json_array.append(v.to_json())
-    return (jsonify(json_array))
+    return(jsonify(amenity.to_json()))
 
 
-@app_views.route('/cities/<string:city_id>',
-                 strict_slashes=False, methods=['GET'])
-def get_amenities_byID(city_id):
-    """ returns state by id """
-    city = storage.get("City", city_id)
-    if city is None:
+@app_views.route('/amenities/<string:amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_amenities_byID(amenity_id=None):
+    """ delete amenity by id"""
+    amenity = storage.get("Amenity", amenity_id)
+    if not amenity:
         abort(404)
-    return(jsonify(city.to_json()))
+    storage.delete(amenity)
+    return jsonify({})
 
 
-@app_views.route('/cities/<string:city_id>/',
-                 strict_slashes=False, methods=['DELETE'])
-def delete_amenities_byID(city_id):
-    """ delete state by id"""
-    city = storage.get("City", city_id)
-    if city is None:
-        abort(404)
-    storage.delete(city)
-    return jsonify({}), 200
+@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
+def post_amenity():
+    """ creates an amenity  """
+    json_obj = None
+    try:
+        json_obj = request.get_json()
+    except:
+        return 'Not a JSON', 400
+
+    if 'name' not in json_obj.keys():
+        return 'Missing name', 400
+    amenity = Amenity(**json_obj)
+    amenity.save()
+    return jsonify(amenity.to_json()), 201
 
 
-@app_views.route('/cities/<string:city_id>/',
-                 strict_slashes=False, methods=['PUT'])
-def put_amenities_byID(city_id):
-    """ update a state by id"""
-    city = storage.get("City", city_id)
-    if city is None:
+@app_views.route('/amenities/<string:amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_amenities_byID(amenity_id=None):
+    """ update an amenityby id"""
+    amenity = storage.get("Amenity", amenity_id)
+    if amenity is None:
         abort(404)
     try:
         request_data = request.get_json()
@@ -62,13 +66,9 @@ def put_amenities_byID(city_id):
         request_data = None
     if request_data is None:
         return "Not a JSON", 404
-    if 'id' in request_data.keys():
-        request_data.pop('id')
-    if 'created_at' in request_data.keys():
-        request_data.pop('created_at')
-    if 'updated_at' in request_data.keys():
-        request_data.pop('updated_at')
+    for item in ("id", "created_at", "updated_at"):
+        request_data.pop(item, None)
     for k, v in request_data.items():
-        setattr(city, k, v)
-    city.save()
-    return jsonify(city.to_json()), 200
+        setattr(amenity, k, v)
+    amenity.save()
+    return jsonify(amenity.to_json()), 200
