7b0a6d4a1369023062b06a8beefa3943443018fc
Stuart Kuredjian <108@holbertonschool.com>
2017-09-14 08:58:57 +0000

    __init__.py :add users import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -13,6 +13,7 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
+from api.v1.views.users import *
 from flask import jsonify
 from models.base_model import BaseModel
 from models.amenity import Amenity
